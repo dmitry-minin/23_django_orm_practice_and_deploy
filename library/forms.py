b1594fe781405eb62eb8ff487382641d2f2832ef
Dmitry Minin <d.n.minin@gmail.com>
2025-05-07 20:45:29 +0200

    added validation

--- library/forms.py
+++ library/forms.py
@@ -12,3 +12,14 @@ class AuthorForm(forms.ModelForm):
     class Meta:
         model = Author
         fields = ['first_name', 'last_name', 'birth_date']
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if Author.objects.filter(first_name=first_name, last_name=last_name).exists():
+            raise forms.ValidationError("Author with this name already exists.")
+
+        return cleaned_data
