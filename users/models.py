f614d77020088a30e733c6835997caecb6dd57f7
Dmitry Minin <d.n.minin@gmail.com>
2025-05-11 13:25:20 +0200

    -authentication for the project: -added new app users -added CustomUser model -migrated -superuser has been created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends the default Django user model.
+    """
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
