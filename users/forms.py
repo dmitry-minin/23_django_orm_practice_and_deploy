758b3c70a6dfc85b2d397a49342603e7a6ec37c5
Dmitry Minin <d.n.minin@gmail.com>
2025-05-13 20:44:54 +0200

    -authentication for the project: -added new app users -added CustomUser model -migrated -superuser has been created - custom command for admin creation created,

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from users.models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=150, required=False, help_text='Введите номер телефона*'
+                                                                             'Поле не является обязательным',)
+    username = forms.CharField(max_length=50, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'first_name', 'last_name', 'phone_number', 'password1', 'password2')
+        widgets = {
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if phone_number and not phone_number.isdigit():
+            raise forms.ValidationError('Номер телефолна должен состоять из цифр')
+        return phone_number
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from users.forms import CustomUserCreationForm
 
-# Create your views here.
+
